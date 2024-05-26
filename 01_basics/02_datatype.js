97a839a3fd67c846d4bd5c4d976e4941d61e324b
miskatbm <miskatbm.78@gmail.com>
2024-05-26 16:50:33 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,32 @@
+"use strict" //treat all js code as newer version
+
+console.log("miskat");
+
+
+console.log(5+2);
+
+
+let nam = "miskat";
+let age = 21;
+let home = "gazipur";
+
+console.table([nam, age, home]);
+
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+
+//object
+
+console.log(typeof "miskat");
+
+console.log(typeof null);//null is object
+
+console.log(typeof undefined);//undefined is undefined
+
